43754c046547aba712505f971fade50a7e8f37b8
hp <hp.tsmc.it@gmail.com>
2023-07-11 15:44:32 +0000

    custom system access crontrol with query

--- src/main/java/io/hpdevelop/trino/systemaccesscontrol/RbacSystemAccessControl.java
+++ src/main/java/io/hpdevelop/trino/systemaccesscontrol/RbacSystemAccessControl.java
@@ -13,6 +13,13 @@
  */
 package io.hpdevelop.trino.systemaccesscontrol;
 
+import io.trino.spi.connector.CatalogSchemaName;
+import io.trino.spi.connector.CatalogSchemaRoutineName;
+import io.trino.spi.connector.CatalogSchemaTableName;
+import io.trino.spi.connector.SchemaTableName;
+import io.trino.spi.eventlistener.EventListener;
+import io.trino.spi.function.FunctionKind;
+import io.trino.spi.type.Type;
 import io.trino.spi.security.Identity;
 import io.trino.spi.security.SystemAccessControl;
 import io.trino.spi.security.SystemSecurityContext;
@@ -24,12 +31,26 @@ import java.util.logging.Logger;
 import java.util.Map;
 import java.util.Optional;
 import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static io.trino.spi.security.AccessDeniedException.denyCreateRole;
 import static io.trino.spi.security.AccessDeniedException.denySetUser;
 import static io.trino.spi.security.AccessDeniedException.denyExecuteQuery;
 import static io.trino.spi.security.AccessDeniedException.denyImpersonateUser;
+import static io.trino.spi.security.AccessDeniedException.denyCatalogAccess;
+import static io.trino.spi.security.AccessDeniedException.denyShowSchemas;
+import static io.trino.spi.security.AccessDeniedException.denyShowTables;
+import static io.trino.spi.security.AccessDeniedException.denyShowColumns;
+import static io.trino.spi.security.AccessDeniedException.denySelectColumns;
 import static java.util.logging.Level.INFO;
+import static java.lang.String.format;
+import static java.util.Collections.emptySet;
 
 public class RbacSystemAccessControl
         implements SystemAccessControl
@@ -51,7 +72,7 @@ public class RbacSystemAccessControl
     public void checkCanCreateRole(SystemSecurityContext context, String role, Optional<TrinoPrincipal> grantor)
     {
         String userName = context.getIdentity().getUser();
-        String logs = "checking if can create role "+role+" to user "+userName;
+        String logs = "(checkCanCreateRole) checking if can create role "+role+" to user "+userName;
         log.log(INFO,logs);
         return;
         // denyCreateRole(userName);
@@ -65,7 +86,7 @@ public class RbacSystemAccessControl
     public void checkCanImpersonateUser(SystemSecurityContext context, String userName)
     {
         String user = context.getIdentity().getUser();
-        String logs = "checking if can impersonate role "+user+" to user "+userName;
+        String logs = "(checkCanImpersonateUser) checking if can impersonate role "+user+" to user "+userName;
         log.log(INFO,logs);
         return;
         //denyImpersonateUser(context.getIdentity().getUser(), userName);
@@ -93,11 +114,161 @@ public class RbacSystemAccessControl
     @Override
     public void checkCanExecuteQuery(SystemSecurityContext context)
     {
-        String nameLog = "user is "+context.getIdentity().getUser();
+        String nameLog = "(checkCanExecuteQuery) user is "+context.getIdentity().getUser();
         log.log(INFO,nameLog);
         if(context.getIdentity().getUser().compareTo("test") != 0)
         {
             denyExecuteQuery();
+        }
+        return;
+    }
+
+    /**
+     * Check if identity is allowed to access the specified catalog
+     *
+     * @throws AccessDeniedException if not allowed
+     */
+    public void checkCanAccessCatalog(SystemSecurityContext context, String catalogName)
+    {
+        String nameLog = "(checkCanAccessCatalog) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            denyCatalogAccess(catalogName);
+        }
+        return;
+        
+    }
+
+    /**
+     * Filter the list of catalogs to those visible to the identity.
+     */
+    public Set<String> filterCatalogs(SystemSecurityContext context, Set<String> catalogs)
+    {
+        String nameLog = "(filterCatalogs) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            return emptySet();
+        }
+        return catalogs;
+    }
+
+    /**
+     * Check if identity is allowed to execute SHOW SCHEMAS in a catalog.
+     * <p>
+     * NOTE: This method is only present to give users an error message when listing is not allowed.
+     * The {@link #filterSchemas} method must filter all results for unauthorized users,
+     * since there are multiple ways to list schemas.
+     *
+     * @throws AccessDeniedException if not allowed
+     */
+    public void checkCanShowSchemas(SystemSecurityContext context, String catalogName)
+    {
+        String nameLog = "(checkCanShowSchemas) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            denyShowSchemas();
+        }
+        return;
+        
+    }
+
+    /**
+     * Filter the list of schemas in a catalog to those visible to the identity.
+     */
+    public Set<String> filterSchemas(SystemSecurityContext context, String catalogName, Set<String> schemaNames)
+    {
+        String nameLog = "(filterSchemas) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            return emptySet();
+        }
+        return schemaNames;
+    }
+
+    /**
+     * Check if identity is allowed to show metadata of tables by executing SHOW TABLES, SHOW GRANTS etc. in a catalog.
+     * <p>
+     * NOTE: This method is only present to give users an error message when listing is not allowed.
+     * The {@link #filterTables} method must filter all results for unauthorized users,
+     * since there are multiple ways to list tables.
+     *
+     * @throws AccessDeniedException if not allowed
+     */
+    public void checkCanShowTables(SystemSecurityContext context, CatalogSchemaName schema)
+    {
+        String nameLog = "(checkCanShowTables) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            denyShowTables(schema.toString());
+        }
+        return;
+    }
+
+    /**
+     * Filter the list of tables and views to those visible to the identity.
+     */
+    public Set<SchemaTableName> filterTables(SystemSecurityContext context, String catalogName, Set<SchemaTableName> tableNames)
+    {
+        String nameLog = "(filterTables) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            return emptySet();
+        }
+        return tableNames;
+    }
+
+    /**
+     * Check if identity is allowed to show columns of tables by executing SHOW COLUMNS, DESCRIBE etc.
+     * <p>
+     * NOTE: This method is only present to give users an error message when listing is not allowed.
+     * The {@link #filterColumns} method must filter all results for unauthorized users,
+     * since there are multiple ways to list columns.
+     *
+     * @throws io.trino.spi.security.AccessDeniedException if not allowed
+     */
+    public void checkCanShowColumns(SystemSecurityContext context, CatalogSchemaTableName table)
+    {
+        String nameLog = "(checkCanShowColumns) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            denyShowColumns(table.toString());
+        }
+        return;
+    }
+
+    /**
+     * Filter the list of columns to those visible to the identity.
+     */
+    public Set<String> filterColumns(SystemSecurityContext context, CatalogSchemaTableName table, Set<String> columns)
+    {
+        String nameLog = "(filterColumns) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            return emptySet();
+        }
+        return columns;
+    }
+
+    /**
+     * Check if identity is allowed to select from the specified columns in a relation.  The column set can be empty.
+     *
+     * @throws AccessDeniedException if not allowed
+     */
+    public void checkCanSelectFromColumns(SystemSecurityContext context, CatalogSchemaTableName table, Set<String> columns)
+    {
+        String nameLog = "(checkCanSelectFromColumns) user is "+context.getIdentity().getUser();
+        log.log(INFO,nameLog);
+        if(context.getIdentity().getUser().compareTo("test") != 0)
+        {
+            denySelectColumns(table.toString(), columns);
         }
         return;
     }
